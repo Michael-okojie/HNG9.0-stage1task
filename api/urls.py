68df10a6ec54381d7a1159f62e81cb3461b802bc
Michael-okojie <michaelokojie1@gmail.com>
2022-10-29 05:18:53 +0100

    updated my urls

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import UserList, UserDetail
 
 urlpatterns = [
-    path('list/', UserList.as_view(), name='list'),
+    path('', UserList.as_view(), name='list'),
     path('Detail/<int:pk>/', UserDetail.as_view(), name='detail'),
 ]
